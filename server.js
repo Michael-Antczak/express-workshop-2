814e88779e3ccad8faa568e6dd903ed911359c6c
Michael Antczak <Michael.Antczak@gmail.com>
2017-07-29 16:58:34 +0100

    done

--- server.js
+++ server.js
@@ -1,10 +1,42 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // The extensions 'html' allows us to serve file without adding .html at the end 
 // i.e /my-cv will server /my-cv.html
 app.use(express.static("public", {'extensions': ['html']}));
 
+app.get('/', function (req, res) {
+    const filePath = __dirname + '/data/posts.json';
+    const callbackFunction = function(error, file) {
+        // we call .toString() to turn the file buffer to a String
+        const fileData = file.toString();
+        // we use JSON.parse to get an object out the String
+        const postsJson = JSON.parse(fileData);
+        // send the json to the Template to render
+        res.render('index', {
+          title: 'Michael Profile', // insert your name instead
+          posts: postsJson
+        });
+    };
+    fs.readFile(filePath, callbackFunction);
+});
+
+app.get( "/admin" , function(req, res) {
+  res.render("admin");
+});
+
+app.get( "/my-cv" , function(req, res) {
+  res.render("my-cv");
+});
+
+app.get( "/contact" , function(req, res) {
+  res.render("contact");
+});
 
 // what does this line mean: process.env.PORT || 3000
 app.listen(process.env.PORT || 3000, function () {
